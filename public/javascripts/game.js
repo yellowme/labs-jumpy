79dd020618ea5da61e5127d7edbfa9d9edd4e8d8
Javier Olan <javikin10@gmail.com>
2015-06-24 17:37:22 -0500

    fix problem with the keypress

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,7 +1,7 @@
 window.addEventListener("load",function() {
   var enemies = [];
   var intervalId;
-  var load = false;
+  var ended = false;
   var Q = window.Q = Quintus()
           .include("Sprites, Scenes, Input, 2D, Anim, Touch, UI")
           .setup({ maximize: true })
@@ -121,29 +121,28 @@ window.addEventListener("load",function() {
     enemies.push(enemy2);
   });
 
+  Q.el.addEventListener("keydown",function(e) {
+    if(ended)    
+        if(e.keyCode == 32 || e.keyCode == 13){        
+          Q.clearStages();
+          Q.stageScene('level1');
+          ended = false;
+        }
+  },false);
+
   // Crea el popup de juego finalizado
   Q.scene('endGame',function(stage) {
+    ended = true;
     var container = stage.insert(new Q.UI.Container({
       x: Q.width/2, y: Q.height/2, fill: "rgba(0,0,0,0.5)"
     }));
     var button = container.insert(new Q.UI.Button({ x: 0, y: 0, fill: "#CCCCCC", label: "Volver a intentar" }))         
     var label = container.insert(new Q.UI.Text({x:10, y: -10 - button.p.h, label: stage.options.label }));
-    // cuando seleccionan el boton se reinicia    
-    
-    if (!load) {
-      load = true;
-      window.on("keydown", function(key){
-        if(key.keyCode == 32 || key.keyCode == 13){
-          Q.clearStages();
-          Q.stageScene('level1');
-        }
-      });
-      
-    }    
-    
+    // cuando seleccionan el boton se reinicia        
     button.on("click",function() {
       Q.clearStages();
       Q.stageScene('level1');
+      ended = false;
     });
 
     clearInterval(intervalId); 
